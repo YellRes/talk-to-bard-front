217688fca1502aae17789bd95b3f256f242ccbe9
zhangtr <zhangtr@greatld.com>
2024-01-22 14:02:25 +0800

    feat: add  eslint husky

--- .umirc.ts
+++ .umirc.ts
@@ -3,7 +3,25 @@ import { defineConfig } from "umi";
 export default defineConfig({
   routes: [
     { path: "/", component: "index" },
-    { path: "/docs", component: "docs" },
+    { path: "/user", component: "user" },
   ],
-  npmClient: 'pnpm',
+  npmClient: "pnpm",
+  extraPostCSSPlugins: [
+    require("postcss-px-to-viewport")({
+      unitToConvert: "px",
+      viewportWidth: 320,
+      unitPrecision: 5,
+      propList: ["*"],
+      viewportUnit: "vw",
+      fontViewportUnit: "vw",
+      selectorBlackList: [],
+      minPixelValue: 1,
+      mediaQuery: false,
+      replace: true,
+      exclude: [],
+      landscape: false,
+      landscapeUnit: "vw",
+      landscapeWidth: 568,
+    }),
+  ],
 });
