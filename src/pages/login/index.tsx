8f062d2b9be4557ebf4817855636fdbf13b03320
zhangtr <zhangtr@greatld.com>
2024-01-31 15:27:05 +0800

    feat: 登录页面模拟支付宝登录50%

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,58 +1,30 @@
-// import { } from ''
-import { useState, useRef, useEffect } from "react";
-import {
-  FloatingPanel,
-  Form,
-  Button,
-  Input,
-  Mask,
-  FloatingPanelRef,
-} from "antd-mobile";
-import { useClickAway } from "ahooks";
+import { useState } from "react";
+import { FloatingPanel, Mask } from "antd-mobile";
 import styles from "./index.less";
+import ConfirmPassword from "./confirmPassword";
+import EnterPassword from "./enterPassword";
+import Register from "./register";
 
-const HEIGHT_MIN = window.innerHeight * 0.4;
-const HEIGHT_MAX = window.innerHeight * 0.8;
-const anchors = [HEIGHT_MIN, HEIGHT_MAX];
+const HEIGHT_MIN = window.innerHeight * 0.43;
+const HEIGHT_MAX = window.innerHeight * 0.7;
+const anchors = [HEIGHT_MIN];
 
 export default function Login() {
   const [isMaskOpen, setMaskOpen] = useState(true);
-  const targetRef = useRef<FloatingPanelRef>(null);
-  const outerRef = useRef<HTMLDivElement>(null);
-
-  useClickAway(() => {
-    setMaskOpen(false);
-    targetRef?.current?.setHeight(HEIGHT_MIN);
-  }, outerRef);
-
-  const handleFocus = () => {
-    setMaskOpen(true);
-    targetRef.current?.setHeight(HEIGHT_MAX);
-  };
-
-  // Input是focus状态，且targetRef此时升高的时候。
-  // useEffect(() => {})
 
   return (
     <>
-      <img src={require("@/assets/bard-login.png")} alt="" width={"100%"} />
+      <img
+        className="mx-auto"
+        src={require("@/assets/chat.png")}
+        alt=""
+        width={"100%"}
+      />
 
-      <div ref={outerRef}>
-        <FloatingPanel ref={targetRef} anchors={anchors}>
-          <Form
-            layout="vertical"
-            footer={
-              <Button block type="submit" color="primary" size="mini">
-                提交
-              </Button>
-            }
-          >
-            <Form.Item name="email" label="邮箱" rules={[{ required: true }]}>
-              <Input placeholder="清输入邮箱" onFocus={handleFocus} clearable />
-            </Form.Item>
-          </Form>
-        </FloatingPanel>
-      </div>
+      {/* 邮箱 => 验证码 => 设置密码 */}
+      <FloatingPanel className="floatingPanel" anchors={anchors}>
+        <Register />
+      </FloatingPanel>
 
       <Mask visible={isMaskOpen} className={styles.login_mask} />
     </>
